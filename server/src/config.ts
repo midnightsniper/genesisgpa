cde56678bd5da5253f27d0dbb4a31e9625f3c77c
midnightsniper <122489945+midnightsniper@users.noreply.github.com>
2025-09-30 20:33:22 -0400

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,8 +1,6 @@
 export const config = {
-  // Render will inject PORT, default to 5177 for local dev
   port: process.env.PORT ? parseInt(process.env.PORT) : 5177,
 
-  // 👇 Add all allowed frontends here
   origin: [
     "https://midnightsniper.github.io",
     "https://midnightsniper.github.io/genesisgpa"
